b7d312c67f1c162555abdb10f0e21d25f16483f4
StanislavPopovSP <staspopov4@gmail.com>
2022-11-27 17:28:07 +0500

    Создаю метод verify_fio для верификации продавца

--- Seller.py
+++ Seller.py
@@ -1,10 +1,27 @@
+import re
+
+
 class Salesman:
 
     # Инициализирую продавца
     def __init__(self, fio):
         self.fio = fio
 
-
+    # Верификация проверки имени, фамилии, отчества
+    @classmethod
+    def verify_fio(cls, fio):
+        if not isinstance(fio, str):
+            # выбрасываем исключение если неправильно введен тип.
+            raise TypeError('ФИО должно быть строкой')
+        data_fio = fio.split()
+        # Пользователь не должен ввести больше или меньше трех значений.
+        if len(data_fio) != 3:
+            raise TypeError('Неверный формат ФИО')
+        # Проверяем что бы не было - и были введены только буквы.
+        letters = ''.join(re.findall(r'[а-яё-]', fio, flags=re.IGNORECASE))
+        for data in data_fio:
+            if len(data.strip(letters)) != 0:
+                raise TypeError('В ФИО можно использовать только буквы и дефис')
 
 
 salesman = Salesman('Марина Сергеевна Петрова')
